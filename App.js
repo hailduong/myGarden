8d1c68f899643613ddcb0658500c6e9fb4861e53
Liano <hailyduong@gmail.com>
2017-12-25 21:54:20 +0700

    feat: render a list of plants from data

--- App.js
+++ App.js
@@ -1,23 +1,131 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, FlatList, Button} from 'react-native';
+import Styles from "./assets/Styles";
+import sortBy from "sort-by";
+
+const initialData = [
+	{
+		id: 1,
+		name: 'Dua Can Hong',
+		lastTime: 1514048400000,
+		type: '5-30-30',
+		amount: 1,
+		interval: 4
+	},
+	{
+		id: 2,
+		name: 'Dua Can Do',
+		lastTime: 1514048400000,
+		type: '5-30-30',
+		amount: 1,
+		interval: 6
+	},
+	{
+		id: 3,
+		name: 'Dua Can Tim',
+		lastTime: 1514048400000,
+		type: '5-30-30',
+		amount: 1,
+		interval: 8
+	},
+	{
+		id: 4,
+		name: 'Hong Thien Huong',
+		lastTime: 1514048400000,
+		type: '12-12-17',
+		amount: 1,
+		interval: 10
+	},
+	{
+		id: 5,
+		name: 'Chuong Vang',
+		lastTime: 1514048400000,
+		type: '12-12-17',
+		amount: 1,
+		interval: 4
+	}
+];
+
+// TODO:
+/*
+ * 1. Sort by remaining days
+ * 2. 
+ * */
+
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+
+	state = {
+		data: {}
+	};
+
+	renderItem = ({item, index}) => {
+
+		return (
+			<View style={[Styles.marginBottomMD]}>
+				<View>
+					<Text>ID: {item.id}</Text>
+					<Text>Name: {item.name}</Text>
+					<Text>Last time: {item.lastTime}</Text>
+					<Text>Remaining: {item.remainingDay} days</Text>
+					<Text>Type: {item.type}</Text>
+					<Text>Amount: {item.amount}</Text>
+					<Text>Interval: {item.interval}</Text>
+				</View>
+				<View style={[Styles.marginTopSM]}>
+					<Button onPress={this.updateItem} title="Update"/>
+				</View>
+			</View>
+		)
+	};
+
+	updateItem = () => {
+		// TODO
+	};
+
+	keyExtractor = (item, index) => {
+		return item.id
+	};
+
+	calculateRemainingDays = (dataArray) => {
+		const currentDate = new Date().getTime();
+		const oneDayTime = 8.64e+7;
+		
+		const dataWithRemainingDays = dataArray.map((item) => {
+			const {lastTime, interval} = item;
+			const remainingDay = interval - Math.floor((currentDate - lastTime) / oneDayTime);
+			return {...item, remainingDay}
+		}).sort(sortBy('remainingDay'));
+
+		return dataWithRemainingDays;
+	};
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<FlatList style={Styles.box} data={this.state.data}
+						  renderItem={this.renderItem}
+						  keyExtractor={this.keyExtractor}
+				/>
+				<View><Text>Footer Text</Text></View>
+			</View>
+		);
+	}
+	
+	componentDidMount() {
+		const dataWithRemainingDays = this.calculateRemainingDays(initialData);
+		console.log('dataWithRemainingDays', dataWithRemainingDays);
+		this.setState({
+			data: dataWithRemainingDays
+		})
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		width: '100%',
+		backgroundColor: '#fff',
+		justifyContent: 'center',
+	}
 });
---
+++ assets/Styles.js
@@ -0,0 +1,24 @@
+import {StyleSheet} from "react-native";
+
+export default StyleSheet.create({
+	marginBottomMD: {marginBottom: 20},
+	marginTopMD: {marginTop: 20},
+	marginTopSM: {marginTop: 10},
+	blueBackground: {backgroundColor: '#2196f3'},
+	blackBackground: {backgroundColor: '#000000'},
+	centerAll: {justifyContent: 'center', alignItems: 'center', flex: 1},
+	flexFull: {flex: 1},
+	fontCormorantInfant: {fontFamily: 'CormorantInfant'},
+	fontCormorantInfantBold: {fontFamily: 'CormorantInfantBold'},
+	fontCormorantInfantLight: {fontFamily: 'CormorantInfantBoldLight'},
+	fontSizeSm: {fontSize: 14},
+	fontSizeNm: {fontSize: 22},
+	fontSizeLg: {fontSize: 24},
+	fontSizeXl: {fontSize: 32},
+	fontSizeXxl: {fontSize: 36},
+	white: {color: '#ffffff'},
+	textCenter: {textAlign: 'center'},
+	textLeft: {textAlign: 'left'},
+	textRight: {textAlign: 'right'},
+	box: {padding: 20}
+})
